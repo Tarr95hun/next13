7100b7737b49e2dba90743936bf6aa159471a9b0
Tarr95hun <30692633+Tarr95hun@users.noreply.github.com>
2023-06-23 12:57:28 +0300

    reservation logic, api, useHook api call, data fetching, state management, loading, new utility function

--- app/restaurant/[slug]/components/ReservationCard.tsx
+++ app/restaurant/[slug]/components/ReservationCard.tsx
@@ -1,22 +1,40 @@
 "use client";
 
 import DatePicker from "react-datepicker";
-import { partySize, times } from "../../../data";
+import { partySize as partySizes, times } from "../../../data";
 import { useState } from "react";
 
 import "react-datepicker/dist/react-datepicker.css";
+import useAvailibility from "../../../../hooks/useAvailibility";
+import { CircularProgress } from "@mui/material";
+import Link from "next/link";
+import {
+  Time,
+  convertToDisplayTime,
+} from "../../../../utils/convertToDisplayTime";
 
 function ReservationCard({
   openTime,
   closeTime,
+  slug,
 }: {
   openTime: string;
   closeTime: string;
+  slug: string;
 }) {
+  const { data, error, loading, fetchAvailabilities } = useAvailibility();
   const [selectedDate, setSelectedDate] = useState<Date | null>(new Date());
+  const [time, setTime] = useState<string>(openTime);
+  const [partySize, setPartySize] = useState<number>(2);
+  const [day, setDay] = useState<string>(
+    new Date().toISOString().split("T")[0]
+  );
+
   const handleDateChange = (date: Date | null) => {
     if (date) {
-      setSelectedDate(date);
+      setDay(date.toISOString().split("T")[0]);
+
+      return setSelectedDate(date);
     }
 
     return setSelectedDate(null);
@@ -46,21 +64,31 @@ function ReservationCard({
     return timesWithinWindow;
   };
 
+  const handleClick = () => {
+    fetchAvailabilities({ slug, day, time, partySize });
+  };
+
   return (
     <div className="w-[27%] relative text-reg">
-      <div className="fixed w-[15%] bg-white rounded p-3 shadow">
+      <div className="fixed w-[20%] min-w-[250px] bg-white rounded p-3 shadow">
         <div className="text-center border-b pb-2 font-bold">
           <h4 className="mr-7 text-lg">Make a Reservation</h4>
         </div>
         <div className="my-3 flex flex-col">
           <label htmlFor="">Party size</label>
-          <select name="" className="py-3 border-b font-light" id="">
-            {partySize.map(({ value, label }) => (
+          <select
+            name=""
+            className="py-3 border-b font-light"
+            id=""
+            value={partySize}
+            onChange={(e) => setPartySize(+e.target.value)}
+          >
+            {partySizes.map(({ value, label }) => (
               <option value={value}>{label}</option>
             ))}
           </select>
         </div>
-        <div className="flex justify-between flex-col">
+        <div className="flex justify-between">
           <div className="flex flex-col w-[48%]">
             <label htmlFor="">Date</label>
             <DatePicker
@@ -70,11 +98,17 @@ function ReservationCard({
               dateFormat="MMMM d"
               wrapperClassName="w-[48%]"
             />
-            <input type="text" className="py-3 border-b font-light w-28" />
+            <input type="text" className="py-3 border-b font-light w-24" />
           </div>
           <div className="flex flex-col w-[48%]">
             <label htmlFor="">Time</label>
-            <select name="" id="" className="py-3 border-b font-light">
+            <select
+              name=""
+              id=""
+              className="py-3 border-b font-light"
+              value={time}
+              onChange={(e) => setTime(e.target.value)}
+            >
               {filterTimesByRestaurantOpenWindow().map(
                 ({ time, displayTime }) => (
                   <option value={time}>{displayTime}</option>
@@ -84,10 +118,40 @@ function ReservationCard({
           </div>
         </div>
         <div className="mt-5">
-          <button className="bg-red-600 rounded w-full px-4 text-white font-bold h-16">
-            Find a Time
+          <button
+            className="bg-red-600 rounded w-full px-4 text-white font-bold h-16"
+            onClick={handleClick}
+            disabled={loading}
+          >
+            {loading ? <CircularProgress /> : "Find a Time"}
           </button>
         </div>
+        {data && data?.length ? (
+          <div className="mt-4">
+            <p className="text-reg">
+              <div className="flex flex-wrap mt-2">
+                {data.map((time) => {
+                  console.log(data);
+
+                  return time?.available ? (
+                    <Link
+                      href={`/reserve/${slug}?date=${day}T${time?.time}&partySize=${partySize}`}
+                      className="bg-red-600 cursor-pointer p-2 w-24 text-center text-white mb-3 rounded mr-3"
+                    >
+                      <p className="text-sm font-bold">
+                        {convertToDisplayTime(time?.time as Time)}
+                      </p>
+                    </Link>
+                  ) : (
+                    <p className="bg-gray-300 p-2 w-24 mb-3 rounded mr-3 text-white text-sm font-bold text-center cursor-not-allowed">
+                      {convertToDisplayTime(time?.time as Time)}
+                    </p>
+                  );
+                })}
+              </div>
+            </p>
+          </div>
+        ) : null}
       </div>
     </div>
   );
---
+++ hooks/useAvailibility.ts
@@ -0,0 +1,53 @@
+import axios from "axios";
+import { useState } from "react";
+
+const useAvailibility = () => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [data, setData] = useState<
+    { time: string; available: boolean }[] | null
+  >(null);
+
+  const fetchAvailabilities = async ({
+    slug,
+    partySize,
+    day,
+    time,
+  }: {
+    slug: string;
+    partySize: number;
+    day: string;
+    time: string;
+  }) => {
+    setLoading(true);
+
+    try {
+      const response = await axios.get(
+        // `http://localhost:3000/api/restaurant/${slug}/availability?day=${day}&time=${time}&partySize=${partySize}`,
+        `http://localhost:3000/api/restaurant/${slug}/availability`,
+        {
+          params: {
+            day,
+            time,
+            partySize,
+          },
+        }
+      );
+
+      setLoading(false);
+      setData(response.data?.availabilities || null);
+    } catch (error: any) {
+      setLoading(false);
+      setError(error?.response?.data?.errorMessage);
+    }
+  };
+
+  return {
+    loading,
+    data,
+    error,
+    fetchAvailabilities,
+  };
+};
+
+export default useAvailibility;
